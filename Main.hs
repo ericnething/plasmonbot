f3a89f893708df0cd124c1f1688de41dbf87ab08
Eric Nething <emnething@gmail.com>
2014-09-20 16:47:22 -0400

    added Net monad using ReaderT to eliminate explicit passing of socket handle

--- Main.hs
+++ Main.hs
@@ -1,8 +1,13 @@
-import Network (connectTo, PortID(..))
+import Network     (connectTo, PortID(..))
 import System.IO
 import System.Exit (exitWith, ExitCode(..))
+
 import Text.Printf (hPrintf, printf)
-import Control.Monad (forever)
+
+import Control.Exception (bracket)
+import Control.Monad     (forever)
+import Control.Monad.Reader
+
 import Data.List (isPrefixOf)
 
 server  = "chat.freenode.net"
@@ -10,29 +15,63 @@ port    = 6667
 channel = "#plasmonbot-testing"
 nick    = "plasmonbot"
 
+-- We are using ReaderT to thread the socket handle throughout the
+-- program to avoid explicity passing it around.
+type Net = ReaderT Bot IO
+
+-- A wrapper for our socket
+data Bot = Bot { socket :: Handle }
+
+
 main :: IO ()
-main = do
+main = bracket connect disconnect loop
+  where loop = runReaderT run
+
+-- Lift a computation from IO into Net
+io :: IO a -> Net a
+io = liftIO
+
+
+-- | Connect to the server
+connect :: IO Bot
+connect = do
   handle <- connectTo server (PortNumber $ fromIntegral port)
   hSetBuffering handle NoBuffering
-  write handle "NICK" nick
-  write handle "USER" $ nick ++ " 0 * :plasmon bot"
-  write handle "JOIN" channel
-  listen handle
+  return (Bot handle)
 
 
--- | Write to the server
-write :: Handle -> String -> String -> IO ()
-write handle cmd input = do
-  hPrintf handle "%s %s\r\n" cmd input
-  printf "> %s %s\r\n" cmd input
+-- | Disconnect from the server
+disconnect :: Bot -> IO ()
+disconnect = hClose . socket
 
 
--- | Listen to the server
-listen :: Handle -> IO ()
+-- | Set the user options for the server and listen on the socket
+run :: Net ()
+run = do
+
+  -- set our user options on the server and join a channel
+  write "NICK" nick
+  write "USER" $ nick ++ " 0 * :plasmon bot"
+  write "JOIN" channel
+
+  -- get the socket handle from Net and listen for activity
+  asks socket >>= listen
+
+
+-- | Write to the socket
+write :: String -> String -> Net ()
+write cmd input = do
+  handle <- asks socket
+  io $ hPrintf handle "%s %s\r\n" cmd input
+  io $ printf "> %s %s\r\n" cmd input
+
+
+-- | Listen to the socket
+listen :: Handle -> Net ()
 listen handle = forever $
 
                 -- capture input from the server
-                hGetLine handle >>=
+                io (hGetLine handle) >>=
 
                 -- drop the last item from the input
                 (\t -> let s = init t in
@@ -41,38 +80,37 @@ listen handle = forever $
                   listenHelper handle s >>
                   
                   -- output the result to the console
-                  putStrLn s)
+                  io (putStrLn s))
 
 
 -- | Route the incoming data to the proper utility
-listenHelper :: Handle -> String -> IO ()
+listenHelper :: Handle -> String -> Net ()
 listenHelper handle input
 
   -- handle server PING events by responding with a PONG event
   | ping input = pong input
 
   -- evaluate all other events
-  | otherwise = eval handle (clean input)
+  | otherwise = eval (clean input)
                 
   where ping x = "PING :" `isPrefixOf` x
-        pong x = write handle "PONG" (':' : drop 6 x)
+        pong x = write "PONG" (':' : drop 6 x)
         clean = drop 1 . dropWhile (/= ':') . drop 1
 
 
 -- | Evaluate messages from other users
-eval :: Handle -> String -> IO ()
+eval :: String -> Net ()
 
 -- allow the bot to quit on command !quit
-eval h "!quit"           = write h "QUIT" ":Exiting" >> exitWith ExitSuccess
+eval "!quit" = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
 
 -- echo back any message prefixed with "!id "
-eval h x
-  | "!id" `isPrefixOf` x = privmsg h $ drop 4 x
+eval x | "!id" `isPrefixOf` x = privmsg $ drop 4 x
 
 -- ignore everything else
-eval _ _                 = return ()
+eval _ = return ()
 
 
 -- | Private message utility
-privmsg :: Handle -> String -> IO ()
-privmsg h s = write h "PRIVMSG" (channel ++ " :" ++ s)
+privmsg :: String -> Net ()
+privmsg s = write "PRIVMSG" (channel ++ " :" ++ s)
