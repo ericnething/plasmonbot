246f0fd7abc57ee390ac9f926c4b45938572e6a7
Eric Nething <emnething@gmail.com>
2014-09-22 20:21:49 -0400

    added the ability to pour beer

--- Main.hs
+++ Main.hs
@@ -9,6 +9,7 @@ import Control.Monad     (forever)
 import Control.Monad.Reader
 
 import Data.List (isPrefixOf)
+import Data.Char (chr, toLower)
 
 server  = "chat.freenode.net"
 port    = 6667
@@ -22,7 +23,6 @@ type Net = ReaderT Bot IO
 -- A wrapper for our socket
 data Bot = Bot { socket :: Handle }
 
-
 main :: IO ()
 main = bracket connect disconnect loop
   where loop = runReaderT run
@@ -31,7 +31,6 @@ main = bracket connect disconnect loop
 io :: IO a -> Net a
 io = liftIO
 
-
 -- | Connect to the server
 connect :: IO Bot
 connect = do
@@ -39,11 +38,9 @@ connect = do
   hSetBuffering handle NoBuffering
   return (Bot handle)
 
-
 -- | Disconnect from the server
 disconnect :: Bot -> IO ()
 disconnect = hClose . socket
-
 
 -- | Set the user options for the server and listen on the socket
 run :: Net ()
@@ -57,7 +54,6 @@ run = do
   -- get the socket handle from Net and listen for activity
   asks socket >>= listen
 
-
 -- | Write to the socket
 write :: String -> String -> Net ()
 write cmd input = do
@@ -65,23 +61,22 @@ write cmd input = do
   io $ hPrintf handle "%s %s\r\n" cmd input
   io $ printf "> %s %s\r\n" cmd input
 
-
 -- | Listen to the socket
 listen :: Handle -> Net ()
-listen handle = forever $
+listen handle =
+  forever $
 
-                -- capture input from the server
-                io (hGetLine handle) >>=
+  -- capture input from the server
+  io (hGetLine handle) >>=
 
-                -- drop the last item from the input
-                (\t -> let s = init t in
+  -- drop the last item from the input
+  (\t -> let s = init t in
 
-                  -- route the data to the proper function
-                  listenHelper handle s >>
-                  
-                  -- output the result to the console
-                  io (putStrLn s))
-
+    -- route the data to the proper function
+    listenHelper handle s >>
+    
+    -- output the result to the console
+    io (putStrLn s))
 
 -- | Route the incoming data to the proper utility
 listenHelper :: Handle -> String -> Net ()
@@ -91,26 +86,44 @@ listenHelper handle input
   | ping input = pong input
 
   -- evaluate all other events
-  | otherwise = eval (clean input)
+  | otherwise = eval (who input) (clean input)
                 
   where ping x = "PING :" `isPrefixOf` x
         pong x = write "PONG" (':' : drop 6 x)
         clean = drop 1 . dropWhile (/= ':') . drop 1
-
+        who   = takeWhile (/= '!') . drop 1
 
 -- | Evaluate messages from other users
-eval :: String -> Net ()
+eval :: String -> String -> Net ()
 
 -- allow the bot to quit on command !quit
-eval "!quit" = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
+eval _ "!quit" = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
 
 -- echo back any message prefixed with "!id "
-eval x | "!id" `isPrefixOf` x = privmsg $ drop 4 x
+eval w x | "!id" `isPrefixOf` x = privmsg $ drop 4 x
+         | "!action" `isPrefixOf` x = actionmsg $ drop 8 x
+         | "!beer" `isPrefixOf` x = actionmsg $ bartender w $ drop 6 x
 
 -- ignore everything else
-eval _ = return ()
+eval _ _ = return ()
 
+-- | Message utility
+message :: String -> Net ()
+message s = write "PRIVMSG" (channel ++ s)
 
 -- | Private message utility
 privmsg :: String -> Net ()
-privmsg s = write "PRIVMSG" (channel ++ " :" ++ s)
+privmsg s = message $ " :" ++ s
+
+-- | Action message utility
+actionmsg :: String -> Net ()
+actionmsg s = message $ ' ':':':(chr 0x1):"ACTION " ++ s ++ ((chr 0x1):[])
+
+-- | Pour a beer
+bartender :: String -> String -> String
+bartender w s =
+  case map toLower s of
+    "guinness" -> pour "Guinness"
+    "murphy's" -> pour "Murphy's"
+    _          -> pour "some random beer"
+  where pour s = "pours " ++ w ++ " a cold pint of " ++ s
